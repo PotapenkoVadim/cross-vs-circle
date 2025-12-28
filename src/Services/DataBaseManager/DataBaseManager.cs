c4dd884fb28f308a98ce1289b7563d7de61e58fd
PotapenkoVadim <potapenko.v00@mail.ru>
2025-12-28 14:48:37 +0300

    refactor: move migration into databasemanager

--- src/Services/DataBaseManager/DataBaseManager.cs
+++ src/Services/DataBaseManager/DataBaseManager.cs
@@ -3,9 +3,27 @@ using Microsoft.Data.Sqlite;
 internal class DataBaseManager: IDisposable
 {
   private readonly SqliteConnection _connection;
+  private const int TARGET_VERSION = 2;
+  private const string DB_FILE = "database.db";
   private bool _dispose = false;
 
-  public DataBaseManager(string dbPath)
+  private static DataBaseManager? _instance;
+
+  public static DataBaseManager Instance
+  {
+    get
+    {
+      if (_instance is null)
+      {
+        string dbPath = Path.Combine(Directory.GetCurrentDirectory(), DB_FILE);
+        _instance = new DataBaseManager(dbPath);
+      }
+
+      return _instance;
+    }
+  }
+
+  private DataBaseManager(string dbPath)
   {
     var directory = Path.GetDirectoryName(dbPath);
     if (!string.IsNullOrWhiteSpace(directory))
@@ -23,6 +41,47 @@ internal class DataBaseManager: IDisposable
     _connection.Open();
 
     ExecuteNonQuery("PRAGMA foreign_key = ON;");
+    ApplyMigrations();
+  }
+
+  private void ApplyMigrations()
+  {
+    int currentVersion = GetVersion();
+
+     if (currentVersion > TARGET_VERSION) throw new Exception($"Migration failed: current version is newer target");
+    if (currentVersion == TARGET_VERSION) return;
+
+    if (currentVersion < 1)
+    {
+      // MIGRATION: Create GameState table
+      ExecuteNonQuery(@"
+      CREATE TABLE IF NOT EXISTS GameState (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        board BLOB NOT NULL,
+        player_x INTEGER NOT NULL CHECK(player_x >= 0 AND player_x < 10),
+        player_y INTEGER NOT NULL CHECK(player_y >= 0 AND player_y < 10),
+        ai_x INTEGER NOT NULL CHECK(ai_x >= 0 AND ai_x < 10),
+        ai_y INTEGER NOT NULL CHECK(ai_y >= 0 AND ai_y < 10),
+        turn INTEGER NOT NULL CHECK (turn IN (1, 0)),
+        player_score INTEGER NOT NULL CHECK(player_score > 0),
+        ai_score INTEGER NOT NULL CHECK(ai_score > 0)
+      );
+      ");
+      SetVersion(1);
+      currentVersion = 1;
+    }
+
+    if (currentVersion < 2)
+    {
+      // MIGRRATION: Add column moves to GameState table
+      ExecuteNonQuery("ALTER TABLE GameState ADD COLUMN moves INTEGER DEFAULT 0;");
+      SetVersion(2);
+      currentVersion = 2;
+    }
+
+    if (currentVersion != TARGET_VERSION)
+      throw new Exception($"Migration failed: current version {currentVersion}");
   }
 
   public int ExecuteNonQuery(string query, Dictionary<string, object>? parameters = null)
--- src/Services/GameLoader/GameLoader.cs
+++ src/Services/GameLoader/GameLoader.cs
@@ -3,17 +3,6 @@ using Microsoft.Data.Sqlite;
 
 internal class GameLoader
 {
-  private const string DB_FILE = "database.db";
-  private const int TARGET_VERSION = 2;
-  private readonly DataBaseManager _dbManager;
-  public GameLoader()
-  {
-    string fullPath = Path.Combine(Directory.GetCurrentDirectory(), DB_FILE);
-    _dbManager = new DataBaseManager(fullPath);
-
-    ApplyMigrations();
-  }
-
   public void Save(GameState state)
   {
     if (state.Board is null)
@@ -32,7 +21,7 @@ internal class GameLoader
       {"@moves", state.Moves}
     };
 
-    _dbManager.ExecuteNonQuery(@"
+    DataBaseManager.Instance.ExecuteNonQuery(@"
     INSERT INTO GameState (
       board, player_x, player_y, ai_x, ai_y, turn, player_score, ai_score, moves
     ) VALUES (
@@ -44,49 +33,9 @@ internal class GameLoader
   public List<Dictionary<string, GameState>> Load()
   {
     string query = "SELECT board, player_x, player_y, ai_x, ai_y, turn, player_score, ai_score, moves, created_at FROM GameState";
-    IEnumerable<Dictionary<string, GameState>> results = _dbManager.Select(query, MapRowToGameState);
+    IEnumerable<Dictionary<string, GameState>> results = DataBaseManager.Instance.Select(query, MapRowToGameState);
 
     return [.. results];
-  }
-
-  private void ApplyMigrations()
-  {
-    int currentVersion = _dbManager.GetVersion();
-
-     if (currentVersion > TARGET_VERSION) throw new Exception($"Migration failed: current version is newer target");
-    if (currentVersion == TARGET_VERSION) return;
-
-    if (currentVersion < 1)
-    {
-      // MIGRATION: Create GameState table
-      _dbManager.ExecuteNonQuery(@"
-      CREATE TABLE IF NOT EXISTS GameState (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-        board BLOB NOT NULL,
-        player_x INTEGER NOT NULL CHECK(player_x >= 0 AND player_x < 10),
-        player_y INTEGER NOT NULL CHECK(player_y >= 0 AND player_y < 10),
-        ai_x INTEGER NOT NULL CHECK(ai_x >= 0 AND ai_x < 10),
-        ai_y INTEGER NOT NULL CHECK(ai_y >= 0 AND ai_y < 10),
-        turn INTEGER NOT NULL CHECK (turn IN (1, 0)),
-        player_score INTEGER NOT NULL CHECK(player_score > 0),
-        ai_score INTEGER NOT NULL CHECK(ai_score > 0)
-      );
-      ");
-      _dbManager.SetVersion(1);
-      currentVersion = 1;
-    }
-
-    if (currentVersion < 2)
-    {
-      // MIGRRATION: Add column moves to GameState table
-      _dbManager.ExecuteNonQuery("ALTER TABLE GameState ADD COLUMN moves INTEGER;");
-      _dbManager.SetVersion(2);
-      currentVersion = 2;
-    }
-
-    if (currentVersion != TARGET_VERSION)
-      throw new Exception($"Migration failed: current version {currentVersion}");
   }
 
   private byte[] MatrixToBlob(CellState[,] matrix)
@@ -131,6 +80,6 @@ internal class GameLoader
   {
     string query = "DELETE FROM GameState WHERE created_at = @created";
     var parameters = new Dictionary<string, object> {{"@created", date}};
-    _dbManager.ExecuteNonQuery(query, parameters);
+    DataBaseManager.Instance.ExecuteNonQuery(query, parameters);
   }
 }
